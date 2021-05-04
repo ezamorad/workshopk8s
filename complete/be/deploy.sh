e52c9e39b72f87d406bdb63ccc8516eaeef7dd88
Edgar Zamora <ezamora@cr.ibm.com>
2021-05-03 22:58:53 -0600

    update complete example

---
+++ complete/be/deploy.sh
@@ -0,0 +1,12 @@
+mvn clean package
+
+docker build -t demoasset-be-complete .
+
+kubectl delete deployment demoasset-be -n demoasset
+
+minikube cache delete demoasset-be-complete:latest
+minikube cache add demoasset-be-complete:latest
+
+kubectl apply -f deployment/k8s/deployment.yaml 
+
+kubectl apply -f deployment/k8s/service.yaml 
